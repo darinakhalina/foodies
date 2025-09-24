1d58fc420b8287fa3100176cf1f5bc3b2e8c0683
Darina Khalina <darina.khalina@namecheap.com>
2025-09-24 16:34:14 +0300

    chore: add button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+import css from './Button.module.css';
+
+const Button = ({
+  children,
+  type = 'button',
+  variant = 'primary',
+  size = 'md',
+  onClick,
+  isDisabled = false,
+  isLoading = false,
+  className,
+}) => {
+  return (
+    <button
+      disabled={isDisabled || isLoading}
+      type={type}
+      className={clsx(
+        css.button,
+        css[variant],
+        css[size],
+        className,
+        {
+          [css['is-loading']]: isLoading,
+          [css['is-disabled']]: isDisabled,
+        }
+      )}
+      onClick={onClick}
+    >
+      {isLoading && (
+        <svg className={css.loader}>
+          <use href="/images/icons.svg#icon-spinner" />
+        </svg>
+      )}
+      <span>{children}</span>
+    </button>
+  );
+};
+
+export default Button;
