f921126ab841c714080ab2409d6482d925456a83
Darina Khalina <darina.khalina@namecheap.com>
2025-09-28 13:13:57 +0300

    fix: clean up test store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { testReducer } from './test/slice';
 
 export const store = configureStore({
-  reducer: {
-    test: testReducer,
-  },
+  reducer: {},
 });
