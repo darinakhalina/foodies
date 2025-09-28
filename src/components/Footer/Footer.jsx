ac1443307024685b7487972e3506f6906b860748
Константин Нестеренко <konstantin.nesterenko@playrix.com>
2025-09-29 00:30:30 +0300

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,46 @@
 import css from './Footer.module.css';
+import { Link } from 'react-router-dom';
+import icons from '/images/icons.svg';
 
 const Footer = () => {
   return (
     <footer className={css.footer}>
-      <div className="f-container">Content here</div>
+      <div className={css.footerTop}>
+        <Link to="/">Foodies</Link>
+        <ul className={css.footerSocials}>
+          <li>
+            <Link to="https://www.facebook.com/goITclub/" target="_blank">
+              <div className={css.footerSocialsItem}>
+                <svg className={css.footerIcon} width="20" height="20">
+                  <use href={`${icons}#icon-facebook`}></use>
+                </svg>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="https://www.instagram.com/goitclub/" target="_blank">
+              <div className={css.footerSocialsItem}>
+                <svg className={css.footerIcon} width="20" height="20">
+                  <use href={`${icons}#icon-instagram`}></use>
+                </svg>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="https://www.youtube.com/c/GoIT/" target="_blank">
+              <div className={css.footerSocialsItem}>
+                <svg className={css.footerIcon} width="20" height="20">
+                  <use href={`${icons}#icon-youtube`}></use>
+                </svg>
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
       <hr className={css.separator} />
-      <div className="f-container">Content here</div>
+      <div className={css.footerBottom}>
+        <p>@2024, Foodies. All rights reserved</p>
+      </div>
     </footer>
   );
 };
