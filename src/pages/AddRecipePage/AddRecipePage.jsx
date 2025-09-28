edfb879ad526e2281da74573395d9b6cf884d612
Darina Khalina <darina.khalina@namecheap.com>
2025-09-28 17:51:20 +0300

    feat: add user info

--- src/pages/AddRecipePage/AddRecipePage.jsx
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,10 +1,17 @@
 import Subtitle from '../../components/Subtitle/Subtitle.jsx';
 import { Link } from 'react-router-dom';
+import PathInfo from '../../components/PathInfo/PathInfo.jsx';
 
 const AddRecipePage = () => {
   return (
     <section className="f-container">
       <Subtitle tag={'h1'}>Add Recipe Page</Subtitle>
+      <PathInfo
+        pages={[
+          { name: 'Home', path: '/' },
+          { name: 'Add Recipe', path: '/recipe/add' },
+        ]}
+      />
       <p>
         <Link to="/" viewTransition>
           Back to HP
