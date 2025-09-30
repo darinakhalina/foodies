f534ebba899a84559fdac21dcd29af1658ee885e
Костянтин Нестеренко <kosnesterenko@gmail.com>
2025-10-01 01:09:45 +0300

    some refactoring

--- src/components/PopularRecipes/PopularRecipes.jsx
+++ src/components/PopularRecipes/PopularRecipes.jsx
@@ -8,36 +8,36 @@ const PopularRecipes = () => {
   const [recipes, setRecipes] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        setLoading(true);
-        const recipesData = await fetchPopularRecipes();
-        setRecipes(recipesData.recipes);
-      } catch (e) {
-        console.log(e);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const fetchRecipes = async () => {
+    try {
+      setLoading(true);
+      const recipesData = await fetchPopularRecipes();
+      setRecipes(recipesData.recipes);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     fetchRecipes();
   }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
 
   return (
     <section className={css.popular}>
       <h2>Popular recipes</h2>
-      <ul>
-        {recipes?.map(el => (
-          <li key={el.id}>
-            <RecipeCard data={el} />
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <Loader />
+      ) : (
+        <ul>
+          {recipes?.map(el => (
+            <li key={el.id}>
+              <RecipeCard data={el} />
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 };
