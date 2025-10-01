08711f9bcc1651d10e87238038dc542c232513c4
oksanaliu <lyuklyan4@icloud.com>
2025-10-01 13:56:27 +0200

    fix photo quality

--- src/utils/cdn.js
+++ src/utils/cdn.js
@@ -6,14 +6,15 @@ const slugify = (name = '') =>
     .replace(/\s+/g, '-')
     .replace(/[^a-z0-9-]/g, '');
 
-export const cdnCategoryImg = (maybeUrl, name, { w = 343, h = 250 } = {}) => {
+export const cdnCategoryImg = (maybeUrl, name) => {
   if (maybeUrl?.includes('/image/upload/')) {
     return maybeUrl.replace(/\/v[^/]+\//, '/');
   }
   const slug = slugify(name);
-  return `https://res.cloudinary.com/${CLOUD}/image/upload/w_${w},h_${h},c_fill,q_auto,f_auto/foodies/categories/${slug}.jpg`;
+  return `https://res.cloudinary.com/${CLOUD}/image/upload/q_auto,f_auto/foodies/categories/${slug}.jpg`;
 };
 
-export const NO_PHOTO = `https://res.cloudinary.com/${CLOUD}/image/upload/w_343,h_250,c_fill,q_auto,f_auto/foodies/placeholders/category.png`;
-export const cdnPlaceholder = (w = 343, h = 250) =>
-  `https://res.cloudinary.com/${CLOUD}/image/upload/w_${w},h_${h},c_fill,q_auto,f_auto/foodies/placeholders/category.png`;
+export const NO_PHOTO = `https://res.cloudinary.com/${CLOUD}/image/upload/q_auto,f_auto/foodies/placeholders/category.png`;
+
+export const cdnPlaceholder = () =>
+  `https://res.cloudinary.com/${CLOUD}/image/upload/q_auto,f_auto/foodies/placeholders/category.png`;
