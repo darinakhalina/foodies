cf86dfbfc435a7b0e283c4feca3c5b5521ad80c1
Darina Khalina <darina.khalina@namecheap.com>
2025-10-02 20:23:15 +0300

    add test

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,20 @@ import Header from '../../components/Header/Header.jsx';
 import Categories from '../../components/Categories/Categories.jsx';
 import Testimonials from '../../components/Testimonials/Testimonials.jsx';
 import css from './HomePage.module.css';
+import { useSearchParams } from 'react-router-dom';
 
 const HomePage = () => {
+  const [params, setParams] = useSearchParams();
+  const category = params.get('category');
+
+  const handleSelect = name => {
+    setParams({ category: name }); // Сюда имя категории
+  };
+
+  const handleClear = () => {
+    setParams({});
+  };
+
   return (
     <>
       <div className={css.homePageUiHolder}>
@@ -17,6 +29,43 @@ const HomePage = () => {
       <section className="f-container">
         <Testimonials />
       </section>
+
+      <div className="f-container">
+        <h2>Test</h2>
+        {!category && (
+          <div>
+            <div>
+              <h2>Секция категорий со списком</h2>
+              <p>Тут список разных карточек</p>
+              <p>Нужно обработать только all - все рецепты без конкретной категории</p>
+              <div>
+                <div>Одна из карточек Beef</div>
+                <div>
+                  <p>Кнопка с переходом на рецепты Beef</p>
+                  <p>
+                    <button onClick={() => handleSelect('Beef')}>Выбрать Beef</button>
+                    <button onClick={() => handleSelect('Dessert')}>Выбрать Dessert</button>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {category && (
+          <div>
+            <h2>У нас выбрана категория: {category}</h2>
+            <p>Мы получаем ее из params.get(category);</p>
+            <p>И делаем запрос на бекенд в рамках этой категории: {category}</p>
+            <div>Тут карточки</div>
+            <p>
+              <button onClick={handleClear}>
+                Очищаем категории и возвращаемся к секции всех категорий
+              </button>
+            </p>
+          </div>
+        )}
+      </div>
     </>
   );
 };
