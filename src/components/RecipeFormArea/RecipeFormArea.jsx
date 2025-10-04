005051e3e71292c9d037296222b99c6a4bac51eb
bit-to-bit <vasiliy.pykhteev@gmail.com>
2025-10-04 12:47:01 +0300

    fix-markup

--- src/components/RecipeFormArea/RecipeFormArea.jsx
+++ src/components/RecipeFormArea/RecipeFormArea.jsx
@@ -4,6 +4,7 @@ import Select from '../Select/Select';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectAreas } from '../../redux/areas/selectors';
 import { getAreas } from '../../redux/areas/operations';
+import css from './RecipeFormArea.module.css';
 
 const RecipeFormArea = () => {
   const dispatch = useDispatch();
@@ -14,7 +15,7 @@ const RecipeFormArea = () => {
   const areas = useSelector(selectAreas);
   return (
     <InputContainer lable="area" id="area">
-      <Select id="area" name="area" items={areas} placeholder="Area" />
+      <Select id="area" name="area" items={areas} placeholder="Area" className={css.area} />
     </InputContainer>
   );
 };
