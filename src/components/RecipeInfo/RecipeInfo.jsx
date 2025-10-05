286d1363a9166bcce2d279e44455e4a19538cccd
Darina Khalina <darina.khalina@namecheap.com>
2025-10-05 15:04:33 +0300

    add fix

--- src/components/RecipeInfo/RecipeInfo.jsx
+++ src/components/RecipeInfo/RecipeInfo.jsx
@@ -14,6 +14,7 @@ const RecipeInfo = () => {
   const [recipe, setRecipe] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  // todo: check fetch
   useEffect(() => {
     const fetchRecipe = async () => {
       try {
