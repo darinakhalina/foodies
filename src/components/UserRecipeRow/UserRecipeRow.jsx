7b8f90be9420670cce198875bc650de704dfdc1a
yaraaos <yara144ss@gmail.com>
2025-10-05 03:25:16 +0300

    fix del

--- src/components/UserRecipeRow/UserRecipeRow.jsx
+++ src/components/UserRecipeRow/UserRecipeRow.jsx
@@ -7,7 +7,6 @@ export default function UserRecipeRow({
   thumb,
   onOpen,
   onDelete,
-  canDelete = false,
 }) {
   return (
     <article className={styles.row}>
@@ -22,14 +21,11 @@ export default function UserRecipeRow({
                 <use href="/images/icons.svg#icon-arrow-up-right" />
               </svg>
             </button>
-
-            {canDelete && ( 
               <button type="button" className={styles.actionBtn} onClick={() => onDelete?.(id)}>
                 <svg className={styles.icon} aria-hidden="true" focusable="false">
                   <use href="/images/icons.svg#icon-trash" />
                 </svg>
               </button>
-            )}
           </div>
         </div>
         <p className={styles.desc}>{description}</p>
